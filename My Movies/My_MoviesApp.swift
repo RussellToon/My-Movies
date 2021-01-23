4070f159332c6e8420d187c011191f6699947648
Russell Toon <russell_toon@yahoo.co.uk>
2021-01-23 12:31:53 +0000

    Initial Commit

---
+++ My Movies/My_MoviesApp.swift
@@ -0,0 +1,17 @@
+//
+//  My_MoviesApp.swift
+//  My Movies
+//
+//  Created by Russell Toon on 23/01/2021.
+//
+
+import SwiftUI
+
+@main
+struct My_MoviesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
